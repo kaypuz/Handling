3a484083ccdc82029b20491b0fd42313cf0bf869
Can Demircan <53577964+kaypuz@users.noreply.github.com>
2023-10-15 12:45:42 +0300

    Test

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+game 'gta5'
+
+
+name "Handling"
+description "Handling Test"
+author "Kaypuz"
+version "1.2.8"
+
+
+files {
+    '*.meta',
+
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+
+
